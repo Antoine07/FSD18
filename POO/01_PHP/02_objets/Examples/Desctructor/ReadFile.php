2c155f27ae38aa7b90c52a63ac845500091a74af
Antoine07 <antoine.lucsko@gmail.com>
2021-10-20 16:57:28 +0200

    read file desctructor

--- POO/01_PHP/02_objets/Examples/Desctructor/ReadFile.php
+++ POO/01_PHP/02_objets/Examples/Desctructor/ReadFile.php
@@ -2,7 +2,7 @@
 
 class ReadFile{
     private string $fileName;
-    private string $resource ;
+    private  $resource ;
 
     public function __construct(string $fileName)
     {
@@ -13,5 +13,11 @@ class ReadFile{
         $this->resource = fopen($this->fileName, "r");
 
         return fread($this->resource, filesize($this->fileName) ) ;
+    }
+
+    public function __destruct()
+    {
+        var_dump("DESTRUCTOR");
+        fclose($this->resource); // fermeture du fichier qui a été ouvert par la méthode read
     }
 }
--- POO/01_PHP/02_objets/Examples/Desctructor/app.php
+++ POO/01_PHP/02_objets/Examples/Desctructor/app.php
@@ -5,6 +5,12 @@ spl_autoload_register(function ($className) {
 });
 
 $readFile = new ReadFile("./data.txt");
+$text = $readFile->read();
 
-$readFile->read();
+var_dump($text);
 
+unset($readFile); // en supprimant => on va déclencher le Destruct on a deux choses 1 libérer la mémoire en supprimant l'objet 2 on a fermé la ressource, le fichier ouvert.
+$sum = 0;
+while($sum < 100) $sum++;
+
+echo "La somme vaut $sum" . "\n";
